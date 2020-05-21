fbd24444f295b16e27a54892bbb06ec5e2f1b776
sobolew <sobolew42@gmail.com>
2020-05-21 13:31:26 +0600

    dsafdadf

--- sp/js/custom.js
+++ sp/js/custom.js
@@ -1,14 +1,11 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
     function onPause(event) {
         console.log('pause', event);
     }
     
     if(Speechki) {
-        Speechki.onload(function() {
-            console.log('customSpeechki', Speechki);
-            Speechki.registerCallback('pause', onPause);
-        })
-        
+        console.log('customSpeechki', Speechki);
+        Speechki.registerCallback('pause', onPause);
     }
 })
 
