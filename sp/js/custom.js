0644d3c5a698d1add4381ee42f4d97c666fdb03c
sobolew <sobolew42@gmail.com>
2020-05-21 19:02:10 +0600

    fasdfasd

--- sp/js/custom.js
+++ sp/js/custom.js
@@ -6,6 +6,11 @@ window.addEventListener('load', function() {
     if(Speechki) {
         console.log('customSpeechki', Speechki);
         Speechki.registerCallback('pause', onPause);
+        Speechki.registerCallback('progress', function (event) {
+            console.log('progress', event);
+        }, {
+            marks: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+        })
     }
 })
 
